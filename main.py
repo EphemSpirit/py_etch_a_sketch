a05ced221ee3c64bf9c1ee1d7e4fa3c7d05b02ec
Drew Hund <languageinvestigator@gmail.com>
2026-03-22 12:26:35 -0400

    super basic etch a sketch

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-# Turtle Event Listeners
-# Turtle listen() starts the turtle listening for user events
 from turtle import Turtle, Screen
 
 jim = Turtle()
 
-
-
-
 def move_forwards() -> None:
     jim.forward(10)
 
+def move_backwards() -> None:
+    jim.backward(10)
 
+def rotate_clockwise() -> None:
+    jim.setheading(jim.heading() - 20)
+
+def rotate_anti_clockwise() -> None:
+    jim.setheading(jim.heading() + 20)
+
+def clear_drawing() -> None:
+    jim.clear()
+    jim.penup()
+    jim.home()
 
 screen = Screen()
-# tell screen to start listening and bind a function to a key press
+
 screen.listen()
-screen.onkey(move_forwards, "space")
+
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(rotate_anti_clockwise, "a")
+screen.onkey(rotate_clockwise, "d")
+screen.onkey(clear_drawing, "c")
+
 screen.exitonclick()
